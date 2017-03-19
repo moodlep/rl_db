bb90dc211224b9ebb1c56eb735a29b42446d27af
Perusha Moodley <perusha.moodley@gmail.com>
2017-03-19 15:53:30 +0000

    added my soln to the policy improvement process.

--- policy_iteration.py
+++ policy_iteration.py
@@ -3,8 +3,6 @@ from gym.envs.rl.gridworld import GridworldEnv
 
 env = GridworldEnv()
 
-
-# Taken from Policy Evaluation Exercise!
 def policy_eval(policy, env, discount_factor=1.0, theta=0.00001):
     """
     Evaluate a policy given an environment and a full description of the environment's dynamics.
@@ -41,7 +39,7 @@ def policy_eval(policy, env, discount_factor=1.0, theta=0.00001):
     return np.array(V)
 
 # Resolve:
-def policy_improvement(env, policy_eval_fn=policy_eval, discount_factor=1.0):
+def policy_improvement_pm(env, policy_eval_fn=policy_eval, discount_factor=1.0):
     """
     Policy Improvement Algorithm. Iteratively evaluates and improves a policy
     until an optimal policy is found.
@@ -63,14 +61,91 @@ def policy_improvement(env, policy_eval_fn=policy_eval, discount_factor=1.0):
     policy = np.ones([env.nS, env.nA]) / env.nA
 
     while True:
-        # Implement this!
-        break
 
-    return policy, np.zeros(env.nS)
+        # get the value function for this policy
+        v = policy_eval(policy, env)
+
+        # set a flag to determine stability of this policy
+        stable_flag = True
+
+        # for each state estimate the action value and decide which action is best for this state:
+        for s, v_s in enumerate(v):
+            q = np.zeros(env.nA)
+            for a, action_prob in enumerate(policy[s]):
+                # For each action, calculate the action-value q
+                for prob, next_state, reward, done in env.P[s][a]:
+                    q[a] = reward + discount_factor * (prob * v[next_state])
+
+            best_action = np.argmax(q)
+            current_action = np.argmax(action_prob)
+
+            if best_action != current_action:
+                # update the policy
+                policy[s] = np.zeros(env.nA)
+                policy[s][best_action] = 1.0
+                # db: use this to det when to stop the loop
+                stable_flag = False
+
+        if stable_flag:
+            break
+
+    return policy, v, q
+
+
+def policy_improvement_db(env, policy_eval_fn=policy_eval, discount_factor=1.0):
+    """
+    Policy Improvement Algorithm. Iteratively evaluates and improves a policy
+    until an optimal policy is found.
+
+    Args:
+        env: The OpenAI envrionment.
+        policy_eval_fn: Policy Evaluation function that takes 3 arguments:
+            policy, env, discount_factor.
+        discount_factor: Lambda discount factor.
+
+    Returns:
+        A tuple (policy, V).
+        policy is the optimal policy, a matrix of shape [S, A] where each state s
+        contains a valid probability distribution over actions.
+        V is the value function for the optimal policy.
+
+    """
+    # Start with a random policy
+    policy = np.ones([env.nS, env.nA]) / env.nA
+
+    while True:
+        # Evaluate the current policy
+        V = policy_eval_fn(policy, env, discount_factor)
+
+        # Will be set to false if we make any changes to the policy
+        policy_stable = True
+
+        # For each state...
+        for s in range(env.nS):
+            # The best action we would take under the currect policy
+            chosen_a = np.argmax(policy[s])
+
+            # Find the best action by one-step lookahead
+            # Ties are resolved arbitarily
+            action_values = np.zeros(env.nA)
+            for a in range(env.nA):
+                for prob, next_state, reward, done in env.P[s][a]:
+                    action_values[a] += prob * (reward + discount_factor * V[next_state])
+            best_a = np.argmax(action_values)
+
+            # Greedily update the policy
+            if chosen_a != best_a:
+                policy_stable = False
+            policy[s] = np.eye(env.nA)[best_a]
+
+        # If the policy is stable we've found an optimal policy. Return it
+        if policy_stable:
+            return policy, V
+
 
 # Tests:
 
-policy, v = policy_improvement(env)
+policy, v , q = policy_improvement_pm(env)
 print("Policy Probability Distribution:")
 print(policy)
 print("")
@@ -87,7 +162,11 @@ print("Reshaped Grid Value Function:")
 print(v.reshape(env.shape))
 print("")
 
-# Test the value function
+print("Action Value Function:")
+print(q)
+print("")
+
+#Test the value function
 expected_v = np.array([ 0, -1, -2, -3, -1, -2, -3, -2, -2, -3, -2, -1, -3, -2, -1,  0])
 np.testing.assert_array_almost_equal(v, expected_v, decimal=2)
 
