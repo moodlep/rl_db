25e52dfda8475efca6fcc5487c94bbae411e63bd
Perusha Moodley <perusha.moodley@gmail.com>
2017-08-22 08:13:24 +0100

    adding card class

---
+++ dom_tests/card.py
@@ -0,0 +1,14 @@
+
+
+class card():
+
+    def __init__(self, name, type, cost, features):
+
+        self.name = name
+        self.type = type
+        self.cost = cost
+        self.features = features
+
+    def get_stats(self):
+
+        return True
